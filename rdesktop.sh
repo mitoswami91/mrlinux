403ca21adc00012306b4f6a598699f050aae56dc
Goswami Mitesh <89632190+mitoswami91@users.noreply.github.com>
2021-08-27 16:09:52 +0530

    Creat new file Rdesktop

---
+++ rdesktop.sh
@@ -0,0 +1,10 @@
+#Resolve hostname in linux 
+echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo tee -a /etc/hosts
+sudo apt-get update -y && sudo apt-get install rdesktop -y && rdesktop
+#/etc/apt/sources.list
+
+#
+## EOL upgrade sources.list
+# deb http://old-releases.ubuntu.com/ubuntu/ precise main restricted universe multiverse
+# deb http://old-releases.ubuntu.com/ubuntu/ precise-updates main restricted universe multiverse
+# deb http://old-releases.ubuntu.com/ubuntu/ precise-security main restricted universe multiverse
